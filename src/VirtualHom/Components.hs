c7e646db17378ab143f4b15e0258cee0c961bdc9
Jann Mueller <j.mueller.11@ucl.ac.uk>
2016-09-11 21:10:51 +0100

    Is this the component?

--- src/VirtualHom/Components.hs
+++ src/VirtualHom/Components.hs
@@ -11,47 +11,52 @@ import VirtualHom.View
 
 import Prelude hiding (div)
 
--- type Layout m a = Cont (Elem (a -> m a) ()) ()
-
 -- Components.
---
--- There are two distinct phases in the execution of a webapp:
--- Rendering, and reacting to callbacks.
--- Rendering is realised as a function `State -> Html`.
--- Callbacks (for an event `E`) are realised as functions `E -> Endo State`.
---
--- Both phases have effects:
--- Rendering reads from the state and creates event handlers.
--- Callbacks modify the state.
---
 -- Components can have an internal state and an external state, just like 
 -- React components have state and props.
 -- 
 
 -- | Component with state `p` (for Props)
 data Component p m = Component {
-  _props :: p,
-  _render :: p -> Elem (Component p m -> m (Component p m)) ()
+  _render :: p -> Elem (p -> m (p, Component p m)) ()
   -- _eventHandlers :: () -- global event handlers for this component, TBD
-} 
+}
 
 makeLenses ''Component
 
-myComp :: Monad m => String -> Component String m 
+data InitialisedComponent p m = InitialisedComponent {
+  _props :: p,
+  _comp :: Component p m
+}
+
+makeLenses ''InitialisedComponent
+
+myComp :: Monad m => Component String m 
 myComp = component "" $ \state props ->
   div & children .~ []
-    -- _ --zoomL innerProps otherComponent
-    -- ]
 
 -- | Create a component with props `p` and inner state `s`
-component :: Functor m => s -> (s -> p -> Elem ((s, p) -> m (s, p)) ()) -> p -> Component p m
-component initialState f prps = Component prps $ inner initialState where
-  inner s p = mapCallbacks (transf s) $ f s p
-  transf s cb comp = fmap (\(s', p') -> component s' f p') $ cb (s, comp^.props) 
+component :: Functor m => s -> (s -> p -> Elem ((s, p) -> m (s, p)) ()) -> Component p m
+component initialState f = Component $ inner initialState where
+  inner s = mapCallbacks (transf s) . f s
+  transf s cb p = fmap (\(s', p') -> (p', component s' f)) $ cb (s, p)
 
-renderComp :: Monad m => Component p m -> [Elem ((Component p m) -> m (Component p m)) ()]
-renderComp comp = [r p] where
-  r = comp^.render
-  p = comp^.props
-  -- transf f cmp = fmap (\p -> cmp & props .~ p) (f (cmp^.props))  
+-- | Render a component inside another component
+within :: Functor m => Component p m -> ((p -> Elem (p -> m (p, Component p m)) ()) -> Component p m) -> Component p m
+within subC f = f render' where
+  render' = fmap (mapCallbacks transf) $ view render subC
+  transf ff p = fmap (\(p', c') -> (p', (c' `within` f))) $ ff p
+
+-- | Generalise a component using a lens
+generalise :: Functor m => Lens' p q -> Component q m -> Component p m
+generalise lns comp = Component render' where
+  render' p = mapCallbacks transf $ (view render comp) (p^.lns)
+  transf cb p = fmap (\(q', comp') -> (p & lns .~ q', generalise lns comp')) $ cb (p^.lns)
+
+-- | Render an `InitialisedComponent`
+renderComp :: Monad m => InitialisedComponent p m -> [Elem ((InitialisedComponent p m) -> m (InitialisedComponent p m)) ()]
+renderComp ic = [mapCallbacks transf $ r initialProps] where
+  initialProps = ic^.props
+  r = ic^.comp.render
+  transf f = fmap (uncurry InitialisedComponent) . f . view props
 
